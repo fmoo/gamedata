62e90610ef13c9b737288a924e6eb38fb8684af6
Peter Ruibal <ruibalp@gmail.com>
2013-12-29 14:35:48 -0800

    Some comments about where dc/rb json comes from

--- src/dancecentral/fetch-songs.py
+++ src/dancecentral/fetch-songs.py
@@ -3,6 +3,7 @@
 
 def main():
     import requests
+    # This json endpoint is fetched by http://www.dancecentral.com/songs
     resp = requests.get('http://www.dancecentral.com/wp-content/themes/turbo/songs-helper.php')
 
     with open('all-songs.json', mode='w') as f:
--- src/rockband/fetch-songs.py
+++ src/rockband/fetch-songs.py
@@ -3,6 +3,7 @@
 
 def main():
     import requests
+    # This json endpoint is fetched by http://www.rockband.com/songs/finder
     resp = requests.get('http://www.rockband.com/services.php/music/all-songs.json')
 
     with open('all-songs.json', mode='w') as f:
